361870505367b5e7bb4b6b7f9757af5955757b50
Andrew0226 <phil1401@ravens.benedictine.edu>
2014-02-06 18:10:19 -0600

    Revert "Created a save unit method/file named Units. The method saves a"
    This reverts commit 4a067043bfd38850a99ac19ab580660302ba2126.

--- src/historicalbattlesimulatorbasic/UnitCreator.java
+++ src/historicalbattlesimulatorbasic/UnitCreator.java
@@ -2,60 +2,45 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
 // */
-package historicalbattlesimulatorbasic;
+//package historicalbattlesimulatorbasic;
 //
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+//import java.util.Scanner;
 //
 ///**
 // *
 // * @author Andrew
-// * @edited: Ed
 // */
-public class UnitCreator 
-{
+//public class UnitCreator 
+//{
 //    
 //    
 //    /**
 //     *
 //     */
-//tester     
-public static void main(String[] args) 
-    {
-    createUnit();
-    }
-    
-   public static Unit createUnit()
-{
-    //What the scanner will say so that user knows what stat they are entering
-       String[] prompt = new String[16];
+//    public static Unit createUnit()
+//    {
+//        //What the scanner will say so that user knows what stat they are entering
+//        String[] prompt = new String[14];
 //        
-        prompt[0]="Enter the name of the Unit";
-        prompt[1]="Enter the type of unit[numerical]";
-        prompt[2]="Enter the damage coefficient";
-        prompt[3]="Enter the attack coefficient";   
-        prompt[4]="Enter the dmgBonus coefficient";        
-        prompt[5]="Enter the hp coefficient";       
-        prompt[6]="Enter the armorClass coefficient";        
-        prompt[7]="Enter the defense coefficient"; 
-        prompt[8]="Enter the speed coefficient";
-        prompt[9]="Enter the range coefficient";
-        prompt[10]="Enter the Chargebonus coefficient";
-        prompt[11]="Enter the stamina coefficient";
-        prompt[12]="Enter the morale coefficient";
-        prompt[13]="Enter the size of the Unit, enter 1 and I will construct"
-                + " an individual soldier instead";
-        prompt[14]="Enter unit x position";
-        prompt[15]="Enter unit y position";
+//        prompt[0]="Enter the name of the Unit";
+//        prompt[1]="Enter the type of unit";
+//        prompt[2]="Enter the damage coefficient";
+//        prompt[3]="Enter the attack coefficient";   
+//        prompt[4]="Enter the dmgBonus coefficient";        
+//        prompt[5]="Enter the hp coefficient";       
+//        prompt[6]="Enter the armorClass coefficient";        
+//        prompt[7]="Enter the defense coefficient"; 
+//        prompt[8]="Enter the speed coefficient";
+//        prompt[9]="Enter the range coefficient";
+//        prompt[10]="Enter the Chargebonus coefficient";
+//        prompt[11]="Enter the stamina coefficient";
+//        prompt[12]="Enter the morale coefficient";
+//        prompt[13]="Enter the size of the Unit, enter 1 and I will construct"
+//                + " an individual soldier instead";
 //
-        System.out.println("Enter in your character information now");
-        System.out.println("At any time, type in exit to leave creation menu");
-        Scanner in= new Scanner(System.in);
+//        System.out.println("Enter in your character information now");
+//        System.out.println("At any time, type in exit to leave creation menu");
+//        Scanner in= new Scanner(System.in);
 //        
 //        
 //        
@@ -67,59 +52,41 @@ public static void main(String[] args)
 //        //think we need ints/doubles unless you plan on adding/subtracting the values
 //        //later on for some reason.  At the moment, i am just typecasting to integer/double
 //        //Regardless, This should work for the moment 
-       String stats[] = new String[16];
-       
-      //loops through to get all the stat coefficient
-       for(int i=0;i<stats.length;i++)
-       {
-           System.out.println(prompt[i]);
-       stats[i]=in.next();
-       }
+//        String[] stats = new String[14];
+//        boolean flag = false;
+//        //loops through to get all the stat coefficient
+//        for(int i=0;i<stats.length;i++)
+//        {
+//            System.out.println(prompt[i]);
+//            stats[i]=in.next();
 //            //if user ever enters exit, it ends
-    /*  if(!stats[i].equals("exit")) 
-            {
-            
-               break;
-           }*/
-       
-     
-           Soldier soldier = new Soldier(
-                   stats[0],Integer.parseInt(stats[1]),Integer.parseInt(stats[2]),Double.parseDouble
-                                      (stats[3]),Double.parseDouble(stats[4]),Double.parseDouble(stats[5]),
-                   Double.parseDouble(stats[6]),Double.parseDouble(stats[7]),
-                   Double.parseDouble(stats[8]),Double.parseDouble(stats[9]),
-                   Double.parseDouble(stats[10]),Double.parseDouble(stats[11]),
-                   Double.parseDouble(stats[12]));
-           Unit unit = new Unit(soldier,
-                   Integer.parseInt(stats[13]),//UnitSize
-                   Integer.parseInt(stats[14]),//Unit x position
-                   Integer.parseInt(stats[15])); //unit y position
-           saveUnit(unit);
-           return unit;
-           }
-           
-       
-    
-      
-       
-
-    private static void saveUnit(Unit unit)
-    {
-        try {
-               unit.saveUnit();
-              
-           } 
-        catch (IOException ex) 
-           {
-               
-           }
-    }
-}
-       
-
-    
-    
-    
-        
-    
-
+//            if("exit".equals(stats[i])) 
+//            {
+//                flag = true;
+//                break;
+//            }
+//                
+//        }
+//        
+//       
+//        if(flag = false) //the user entered all stats without exiting
+//        {
+//            
+//            Soldier soldier = new Soldier(
+//                stats[0],Integer.parseInt(stats[1]),Integer.parseInt(stats[2]),Double.parseDouble
+//                (stats[3]),Double.parseDouble(stats[4]),Double.parseDouble(stats[5]),
+//                Double.parseDouble(stats[6]),Double.parseDouble(stats[7]),
+//                Double.parseDouble(stats[8]),Double.parseDouble(stats[9]),
+//                Double.parseDouble(stats[10]),Double.parseDouble(stats[11]),
+//                Double.parseDouble(stats[12]));
+//            Unit unit = new Unit(soldier,Integer.parseInt(stats[13]));
+//            return unit;
+//        }
+//        else
+//        {
+//            return null;
+//        }
+//        
+//      
+//    }   
+//}
