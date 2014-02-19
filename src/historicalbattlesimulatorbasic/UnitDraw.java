9bf4dbcc0461e398b08f079574aa0f38005b3412
Andrew0226 <phil1401@ravens.benedictine.edu>
2014-02-18 21:57:36 -0600

    UnitDraw

--- src/historicalbattlesimulatorbasic/UnitDraw.java
+++ src/historicalbattlesimulatorbasic/UnitDraw.java
@@ -21,30 +21,18 @@ public class UnitDraw extends Rectangle
     int heightY = 30;
     int widthX = 50;
     int xDraw,yDraw;
-    static int numberArray=0;
     public UnitDraw(int x, int y)
     {
         super.setBounds(x,y,height,width);
         this.xDraw=x;
         this.yDraw=y;
-        numberArray++;
     }
     //just add where they need to be painted and this method will do it
     public void paint(Graphics g)
-<<<<<<< HEAD
     { 
         Graphics2D g2 = (Graphics2D) g;
         g2.fill(this);
-=======
-    {
-//   Graphics2D g2 = (Graphics2D) g;
-//        Rectangle rect[] = new Rectangle[2];
-//        
-//        for (Rectangle rect1 : rect) 
-//            g2.fill(rect1);
-//        
->>>>>>> 054f44db2d71d5b4f42d28013470d9d0e598ae54
-        
+
     }
 
 }
