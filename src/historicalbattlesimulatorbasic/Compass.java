0f46c9d2170298fff19c1b6e565c343b7f1084e6
Andrew0226 <phil1401@ravens.benedictine.edu>
2014-04-03 18:19:17 -0500

    Compass Refactored

--- src/historicalbattlesimulatorbasic/Compass.java
+++ src/historicalbattlesimulatorbasic/Compass.java
@@ -94,36 +94,41 @@ public final class Compass extends JPanel
    
     public Compass()
     {   
-       this.north.setOpaque(false);
-       this.north.setContentAreaFilled(false);
-       this.north.setBorderPainted(false);
-       this.northEast.setOpaque(false);
-       this.northEast.setContentAreaFilled(false);
-       this.northEast.setBorderPainted(false);
-       this.northWest.setOpaque(false);
-       this.northWest.setContentAreaFilled(false);
-       this.northWest.setBorderPainted(false);
-       this.east.setOpaque(false);
-       this.east.setContentAreaFilled(false);
-       this.east.setBorderPainted(false);
-       this.west.setOpaque(false);
-       this.west.setContentAreaFilled(false);
-       this.west.setBorderPainted(false);
-       this.south.setOpaque(false);
-       this.south.setContentAreaFilled(false);
-       this.south.setBorderPainted(false);
-       this.southEast.setOpaque(false);
-       this.southEast.setContentAreaFilled(false);
-       this.southEast.setBorderPainted(false);
-       this.southWest.setOpaque(false);
-       this.southWest.setContentAreaFilled(false);
-       this.southWest.setBorderPainted(false);
-       this.toggleSprint.setOpaque(false);
-       this.toggleSprint.setContentAreaFilled(false);
-       this.toggleSprint.setBorderPainted(false);
-       setOpaque(false);
+        removeJButtonDefaults();
+       
       // comp.setLayout(new GridLayout(3,3));
       // this.setUpVisuals();
+    }
+
+    protected void removeJButtonDefaults() {
+        this.north.setOpaque(false);
+        this.north.setContentAreaFilled(false);
+        this.north.setBorderPainted(false);
+        this.northEast.setOpaque(false);
+        this.northEast.setContentAreaFilled(false);
+        this.northEast.setBorderPainted(false);
+        this.northWest.setOpaque(false);
+        this.northWest.setContentAreaFilled(false);
+        this.northWest.setBorderPainted(false);
+        this.east.setOpaque(false);
+        this.east.setContentAreaFilled(false);
+        this.east.setBorderPainted(false);
+        this.west.setOpaque(false);
+        this.west.setContentAreaFilled(false);
+        this.west.setBorderPainted(false);
+        this.south.setOpaque(false);
+        this.south.setContentAreaFilled(false);
+        this.south.setBorderPainted(false);
+        this.southEast.setOpaque(false);
+        this.southEast.setContentAreaFilled(false);
+        this.southEast.setBorderPainted(false);
+        this.southWest.setOpaque(false);
+        this.southWest.setContentAreaFilled(false);
+        this.southWest.setBorderPainted(false);
+        this.toggleSprint.setOpaque(false);
+        this.toggleSprint.setContentAreaFilled(false);
+        this.toggleSprint.setBorderPainted(false);
+        setOpaque(false);
     }
         
     public void init() {
@@ -234,7 +239,7 @@ public final class Compass extends JPanel
     public void moveLogic()
     {
        GUI.unitSelected.hasMoved=true;
-       int index=Integer.MAX_VALUE;
+       int index=-1;
        
        //find the index of the unitDraw that needs to be removed
        for(int i=0;i<GUI.unitDraws.size();i++)
@@ -245,8 +250,13 @@ public final class Compass extends JPanel
               System.out.println("unitID of unitDraw at "+ i+ " = "+
                       GUI.unitDraws.get(i).thisUnit.getUnitID());
               index=i;
+              break;
           }
        }
+       if(index==-1)
+           System.out.println("never found the right index in compass");
+           
+       
        
        int tileMoveChange = GUI.tileWidth;
        
@@ -263,77 +273,15 @@ public final class Compass extends JPanel
        //not inherienting speed from soldiers, needs to be fixed. should be able to call units speed not a soldiers speed
       if(GUI.unitSelected.unitSoldiers[0].speed>0)
       {
-         switch(moveDirection)
-         {
-           case 1: //north
-           {
-               GUI.unitSelected.yPosition-=tileMoveChange; //yPosition - 10 = 1 tile north
-               break;
-           }
-           case 2: //northEast
-           {
-//               System.out.println("northEast in logic");
-               GUI.unitSelected.xPosition+=tileMoveChange;//xPosition + 10 = 1 tile east
-               GUI.unitSelected.yPosition-=tileMoveChange; //yPosition - 10 = 1 tile north
-               break;
-           }
-           case 3: //East
-           {
-//               System.out.println("east in logic");
-               GUI.unitSelected.xPosition+=tileMoveChange;//xPosition + 10 = 1 tile east
-               break;
-           }
-           case 4: //southEast
-           {
-//               System.out.println("southEast in logic");
-               GUI.unitSelected.xPosition+=tileMoveChange;//xPosition + 10 = 1 tile east
-               GUI.unitSelected.yPosition+=tileMoveChange;//yPosition + 10 = 1 tile South
-               break;
-           }
-           case 5: //South
-           {
-//               System.out.println("south in logic");
-               GUI.unitSelected.yPosition+=tileMoveChange;//yPosition + 10 = 1 tile South
-               break;
-           }
-           case 6://southWest
-           {
-//               System.out.println("southWest in logic");
-               GUI.unitSelected.xPosition-=tileMoveChange;//xPosition -10 = 1 tile West
-               GUI.unitSelected.yPosition+=tileMoveChange;//yPosition + 10 = 1 tile South
-               break;
-           }
-           case 7://West
-           {
-//               System.out.println("west in logic");
-               GUI.unitSelected.xPosition-=tileMoveChange;//xPosition -10 = 1 tile West
-               break;
-           }
-           case 8://NorthWest
-           {
-//               System.out.println("northWest in logic");
-               GUI.unitSelected.xPosition-=tileMoveChange;//xPosition -10 = 1 tile West
-               GUI.unitSelected.yPosition-=tileMoveChange;//yPosition -10 = 1 tile North
-               break;
-           }
-       }
-         //this set the unit onto the tile
-          GUI.unitSelected.setPosition(GUI.unitSelected.xPosition,GUI.unitSelected.yPosition);
+         
+          //figure out where the new unit will be located
+          determineTheNewLocationOfTheUnit(tileMoveChange);
           
-          //remove the soldiers from the previous tiles
-          for(int i=0;i<GUI.numberOfTilesHeight;i++)
-              for(int j=0;j<GUI.numberOfTilesWidth;j++)
-              {
-                  if(GUI.tileGameMap[j][i].getOccupier()!=null&&GUI.tileGameMap[j][i].getOccupier().getUnitID()==GUI.unitSelected.getUnitID())
-                      GUI.tileGameMap[j][i].occupyingSoldier=null;
-              }
+          removeSoldiersFromPreviousTiles();
           
           //updates the draw to show new location of unit
             UnitDraw draw = new UnitDraw(GUI.unitSelected,new Tile(GUI.unitSelected.xPosition,GUI.unitSelected.yPosition,GUI.tileWidth,GUI.tileWidth));
-            if(GUI.unitSelected.currentFormation!=null)
-            {
-                 System.out.println("GUI unitSelected formation != null in compass");
-            }
+            
             GUI.unitDraws.add(draw);//adds the new unit
             GUI.unitDraws.remove(index); //removes the previous unit
             moveDirection=0; //no direction
@@ -342,6 +290,80 @@ public final class Compass extends JPanel
             
       } 
    }
+
+    protected void removeSoldiersFromPreviousTiles() {
+        //remove the soldiers from the previous tiles
+        for(int i=0;i<GUI.numberOfTilesHeight;i++)
+            for(int j=0;j<GUI.numberOfTilesWidth;j++)
+            {
+                if(thereIsASoldierWhereThereShouldNotBe(j, i))
+                    GUI.tileGameMap[j][i].occupyingSoldier=null;
+            }
+    }
+
+    protected boolean thereIsASoldierWhereThereShouldNotBe(int j, int i) {
+        return GUI.tileGameMap[j][i].getOccupier()!=null&&GUI.tileGameMap[j][i].getOccupier().getUnitID()==GUI.unitSelected.getUnitID();
+    }
+
+    protected void determineTheNewLocationOfTheUnit(int tileMoveChange) {
+        switch(moveDirection)
+        {
+            case 1: //north
+            {
+                GUI.unitSelected.yPosition-=tileMoveChange; //yPosition - 10 = 1 tile north
+                break;
+            }
+            case 2: //northEast
+            {
+//               System.out.println("northEast in logic");
+                GUI.unitSelected.xPosition+=tileMoveChange;//xPosition + 10 = 1 tile east
+                GUI.unitSelected.yPosition-=tileMoveChange; //yPosition - 10 = 1 tile north
+                break;
+            }
+            case 3: //East
+            {
+//               System.out.println("east in logic");
+                GUI.unitSelected.xPosition+=tileMoveChange;//xPosition + 10 = 1 tile east
+                break;
+            }
+            case 4: //southEast
+            {
+//               System.out.println("southEast in logic");
+                GUI.unitSelected.xPosition+=tileMoveChange;//xPosition + 10 = 1 tile east
+                GUI.unitSelected.yPosition+=tileMoveChange;//yPosition + 10 = 1 tile South
+                break;
+            }
+            case 5: //South
+            {
+//               System.out.println("south in logic");
+                GUI.unitSelected.yPosition+=tileMoveChange;//yPosition + 10 = 1 tile South
+                break;
+            }
+            case 6://southWest
+            {
+//               System.out.println("southWest in logic");
+                GUI.unitSelected.xPosition-=tileMoveChange;//xPosition -10 = 1 tile West
+                GUI.unitSelected.yPosition+=tileMoveChange;//yPosition + 10 = 1 tile South
+                break;
+            }
+            case 7://West
+            {
+//               System.out.println("west in logic");
+                GUI.unitSelected.xPosition-=tileMoveChange;//xPosition -10 = 1 tile West
+                break;
+            }
+            case 8://NorthWest
+            {
+//               System.out.println("northWest in logic");
+                GUI.unitSelected.xPosition-=tileMoveChange;//xPosition -10 = 1 tile West
+                GUI.unitSelected.yPosition-=tileMoveChange;//yPosition -10 = 1 tile North
+                break;
+            }
+            
+        }
+         GUI.unitSelected.setPosition(GUI.unitSelected.xPosition,GUI.unitSelected.yPosition);
+
+    }
        
 }
 
